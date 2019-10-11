f56d22d550abfe9188ce9fad9ad4439e672ce472
lorris97 <33892942+lorris97@users.noreply.github.com>
2019-10-11 17:02:48 +0200

    Add files via upload

---
+++ DemoMockitoV3/src/test/java/V3MedecinServiceTest.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import BO.MedecinBO;
+import DAO.ImedecinDAO;
+import Service.MedecinService;
+import java.sql.SQLException;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+
+/**
+ *
+ * @author lorris
+ */
+public class V3MedecinServiceTest {
+    
+    public V3MedecinServiceTest() {
+    }
+    
+        @Mock
+    ImedecinDAO dao;
+ 
+@InjectMocks MedecinService service ;
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    
+    
+    
+    
+    
+    @org.junit.jupiter.api.Test
+    public void testMockito() throws SQLException 
+    {
+        MockitoAnnotations.initMocks(this);
+        
+        
+        
+        MedecinBO bo = new MedecinBO();
+        bo.setPrenom("test");
+        
+        when(dao.getElementById(1)).thenReturn(bo);
+        
+        MedecinBO boTest = service.getElementById(1);
+        verify(dao, times(1)).getElementById(1);
+
+       // Assertions.assertEquals(bo.getPrenom(),boTest.getPrenom());
+                
+        
+        
+    }
+}
